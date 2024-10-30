e571e91bbaa0066bb1eabc41e8d16a769362860a
Savera-saleem123 <siddiquisawera1@gmail.com>
2024-10-30 12:36:32 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,26 @@
+
+x=12 #num1
+y=8 #num2
+z=2 #num3
+S=x+y+z #sum
+print(S)
+Avg=S/3 #average
+print(Avg)
+D=x-y-z #diffrences
+print(D)
+P=x*y*z #product
+print(P)
+b=5 #base in unit
+h=6 #height in unit
+A=1/2*5*6 #area of triangle in square units
+print(A)
+s=5 #sides lenght in units
+a=s*s #area of square in square units
+print(a)
+r=6
+c=22/7*r**2 #area of circle in square units
+print(c) 
+l=8 #lenght in units
+w=5 #width in units
+R=l*w #area of rectangle in square units
+print(R)
